48dcc230c4f1a24a2d2aec4d7a0ffde75b626112
Muhammad Rashidov <mrashidov@outlook.com>
2020-11-24 22:11:26 +0500

    [Hojiabror sizga pushillatyabman pullilatib oling]

---
+++ LMSWebMVC/Sibov.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMSWebMVC
+{
+    public class Sibov
+    {
+        //git ni ishlashini ko'rsatish uchun
+    }
+}
