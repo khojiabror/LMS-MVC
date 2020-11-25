40d8bf8d26999863d0eaf3e94274b42625726ef9
Muhammad Rashidov <mrashidov@outlook.com>
2020-11-25 09:03:58 +0500

    z

---
+++ LMSWebMVC/Services/Business/SecurityService.cs
@@ -0,0 +1,19 @@
+﻿using LMSWebMVC.Models;
+using LMSWebMVC.Services.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMSWebMVC.Services.Business
+{
+    public class SecurityService
+    {
+        SecurityDAO daoService = new SecurityDAO();
+
+        public bool Authenticate(UserModel user)
+        {
+            return daoService.FindByUser(user);
+        }
+    }
+}
---
+++ LMSWebMVC/Services/Data/SecurityDAO.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using LMSWebMVC.Models;
+
+namespace LMSWebMVC.Services.Data
+{
+    public class SecurityDAO
+    {
+        //connectString to the database
+        string connectionString = "Data Source=10.10.10.3;Initial Catalog=DBLMS;User ID=LMSuser;Password=********;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        internal bool FindByUser(UserModel user)
+        {
+            //if(user.Username=="Admin" && user.Password == "Secret")
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            bool success = false;
+
+            string queryString = "SELECT * FROM USERS WHERE username=@Username AND password=@Password";
+
+            using(SqlConnection connection=new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@Username", System.Data.SqlDbType.VarChar, 50).Value = user.Username;
+                command.Parameters.Add("@Password", System.Data.SqlDbType.VarChar, 100).Value = user.Password;
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        success = true;
+                    }
+                    else
+                    {
+                        success = false;
+                    }
+                    reader.Close();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+        }
+    }
+}
